80e6532ef9633a621495666e60a44f09423e51de
Kevin Urrutia <kevinurrutia@pop-os.localdomain>
2022-05-04 10:37:28 -0700

    added working gaussian filtering code

--- sift.py
+++ sift.py
@@ -1,20 +1,16 @@
 import cv2 as cv
 import numpy as np
 from numba import jit
+from itertools import product
+
+def gen_gaussian_kernel(k_size, sigma):
+    center = k_size // 2
+    x, y = np.mgrid[0 - center : k_size - center, 0 - center : k_size - center]
+    g = 1 / (2 * np.pi * sigma) * np.exp(-(np.square(x) + np.square(y)) / (2 * np.square(sigma)))
+    return g
 
 @jit()
-def gaussian_blur(img):
-    #the kernel has the convoluted with every pixel in the image
-    #The convolution operation is by specifying a central pixel coordinate and multiplying all of its nieghbors with the kernel. Then adding up all of the resulting multiplications and
-    #placing those values in the original pixel location
-    #There needs to be boundary condition checks when doing the covolution
-
-    kernel = np.array([[1, 4, 6, 4, 1],
-                       [4, 16, 24, 16, 4],
-                       [6, 24, 36, 24, 6],
-                       [4, 16, 24, 16, 4],
-                       [1, 4, 6, 4, 1]])
-
+def gaussian_filter(img, k_size, sigma):
     [rows, cols] = img.shape
 
     #create a resulting image to return from the function
@@ -24,31 +20,27 @@ def gaussian_blur(img):
     padded_image = np.zeros([rows + 4, cols + 4])
     padded_image[int(2):int(-1 * 2), int(2):int(-1 * 2)] = img
 
-    #iterate through the image to do the convolution
+    kernel = gen_gaussian_kernel(k_size, sigma)
+    print(kernel)
+    # iterate through the image to do the convolution
     for y in range(cols):
         for x in range(rows):
-            print(padded_image[x: x + 5, y: y + 5])
             blur_img[x, y] = ((kernel * padded_image[x: x+5, y: y + 5]).sum()) / 256
 
-    print(blur_img)
     return blur_img
 
+
 def main():
-    img = np.array([[1, 4, 6, 4, 1],
-                   [4, 16, 24, 16, 4],
-                   [6, 24, 36, 24, 6],
-                   [4, 16, 24, 16, 4],
-                   [1, 4, 6, 4, 1]])
+    img = cv.imread("assets/pumpkins.jpg", cv.IMREAD_GRAYSCALE)
+    cv.imshow("orig", img)
 
-    # img = cv.imread("assets/pumpkins.jpg", cv.IMREAD_GRAYSCALE)
-    # cv.imshow("orig", img)
-
-    # dst = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT)
-
-    blur_img = gaussian_blur(img)
-    # cv.imshow('blurred', blur_img)
+    # gaussian3x3 = gaussian_filter(img, 3, sigma=1)
+    gaussian5x5 = gaussian_filter(img, 5, sigma=0.8)
     #create the scaled space for the image
         #entails blurring the image and shrinking the image for a number of octaves
+
+    # cv.imshow("gaussian filter with 3x3 mask", gaussian3x3)
+    cv.imshow("gaussian filter with 5x5 mask", gaussian5x5)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
